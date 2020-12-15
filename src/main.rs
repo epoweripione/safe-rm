980c180687b5a3bacfc09b677d9a065b9f298860
Francois Marier <francois@fmarier.org>
2020-12-14 22:12:17 -0800

    Forbid all unsafe code to make cargo-geiger happy.

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+#![forbid(unsafe_code)]
+
 use glob::glob;
 use std::fs::{self, File};
 use std::io::{self, BufRead};
